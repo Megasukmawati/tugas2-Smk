9256e8a2bc1d718d01d0ebf8c25a25f2f1efe3ff
Megasukmawati <megasukmawati60@gmail.com>
2022-02-03 14:38:32 +0700

    commit pertama

---
+++ tgs.2.js
@@ -0,0 +1,11 @@
+function tgs(no1,no2) {
+    if (no1 + no2 <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(tgs(22,15));
+console.log(tgs(83,34));
+console.log(tgs(3,77));
---
+++ tgs3.js
@@ -0,0 +1,11 @@
+function comp(string1, string2) {
+    if (string1.lenght === string2.lenght) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(comp("AB", "CD"))
+console.log(comp("EFs", "GHt"))
+console.log(comp("IJo", "Bi"))
